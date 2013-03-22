b0593580b407d015e401154101db38db12e4c1c2
Alex Jarvis <alxjrvs@gmail.com>
2013-03-22 16:37:01 -0400

    initial phase of JSON api done

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,22 @@
 class CardsController < ActionController::Base
   def show
-    @card = Card.where(name: card_params[:card_name]).first
+    if (@card = Card.where(name: card_params[:card_name]).first)
+      render action: :show, status: 201
+    else
+      render text: '{"error" : "Card does not exist.}', status: 404
+    end
   end
 
   def detail_show
+    if (@card = Card.where(name: card_params[:card_name]).first)
+      if (@printing = @card.printings.where(expansion_id: Expansion.where(short_name: card_params[:expansion_shortname]).first.id).first)
+        render action: :show, status: 201
+      else
+        render text: '{"error": "Card Exists but does not belong to expansion with given short name."}', status: 404
+      end
+    else
+      render text: '{"error" : "Card does not exist.}', status: 404
+    end
   end
 
   private
---
+++ app/views/cards/show.json.jbuilder
@@ -0,0 +1,21 @@
+json.name @card.name
+json.cost @card.cost
+json.super_types @card.super_types
+json.card_types @card.card_types
+json.sub_types @card.sub_types
+json.card_text @card.card_text
+json.colors @card.colors
+json.color_indicator @card.color_indicator
+json.power @card.power
+json.toughness @card.toughness
+
+if @printing.present?
+  json.raw_text @printing.raw_text
+  json.rules_text @printing.rules_text
+  json.rarity @printing.rarity
+  json.illustrator @printing.illustrator
+  json.flavor_text @printing.flavor_text
+  json.watermark @printing.watermark
+  json.card_number @printing.card_number
+  json.multiverse_number @printing.multiverse_number
+end
